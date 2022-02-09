18f73063891284099e3b647c83f5354e767179ee
Yousif Shafee <yousiftariq1@hotmail.com>
2022-02-09 02:54:39 +0200

    Convert Unix String To Integer

--- server.js
+++ server.js
@@ -22,10 +22,15 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/:date", function (req, res) {
   var date = req.params.date;
-  var time = new Date(date);
+  if(/-/.test(date)){
+    var time = new Date(date);
+  } else {
+    var time = new Date(parseInt(date));
+  }
   if(time == 'Invalid Date'){
     res.json({error: 'Invalid Date'})
-  } else {
+  }
+  else{
     res.json({unix: time.getTime(), utc: time.toUTCString()})
   }
 });
